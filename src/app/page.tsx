94497c7f894712eb5e227f22e579f275731c35ae
Yacine Kahlerras <kahlerasse@gmail.com>
2026-02-02 17:34:34 +0100

    fix: remove random class from main container in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import FeaturedSection from "@/components/featured";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen random-class">
+    <div className="flex flex-col min-h-screen">
       <Nav />
       <Hero />
       <FeaturedSection />
