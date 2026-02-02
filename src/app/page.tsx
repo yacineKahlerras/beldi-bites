bc778688cba506c1258e352a187decca7037f2c9
Yacine Kahlerras <kahlerasse@gmail.com>
2026-02-02 17:30:21 +0100

    feat: add random class to main container in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import FeaturedSection from "@/components/featured";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen random-class">
       <Nav />
       <Hero />
       <FeaturedSection />
